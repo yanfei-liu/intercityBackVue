ad579bd6390d8eda8bb9e7f9e82d01f7ccb96ef1
xuese2019 <15100162730@163.com>
2019-05-05 22:31:48 +0800

    123

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+	router,
+	render: h => h(App)
+}).$mount('#app');
